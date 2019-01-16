a737be741d8d16c11c308f35b6bf3b8190c2a627
sunyonghua <yonghuasir@icloud.com>
2019-01-16 13:50:32 +0800

    安装babel7

---
+++ src/main.tsx
@@ -0,0 +1,4 @@
+console.log(1);
+let a = 1;
+
+new Promise((resolve, reject) => console.log(1));
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,43 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
-  entry: './src/main.js',
+  entry: './src/main.tsx',
+  stats: {
+    // 添加资源信息
+    assets: true,
+    // 显示缓存的资源（将其设置为 `false` 则仅显示输出的文件）
+    cachedAssets: false,
+    // 添加 chunk 信息（设置为 `false` 能允许较少的冗长输出）
+    chunks: false,
+    // 添加构建模块信息
+    modules: false,
+    // 添加 children 信息
+    children: false
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'react-template',
+      filename: 'index.html',
+      template: 'index.html',
+      minify: {
+        // 压缩HTML文件
+        removeComments: true, //移除HTML中的注释
+        collapseWhitespace: true //删除空白符与换行符
+      }
+    })
+  ]
 };
